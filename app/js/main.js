5ecfe42c67e5a86f0018312489e4389c085192aa
Paulo Castillo <Paulo.Castillo@prodigious.com>
2015-08-07 13:21:38 -0600

    Add new styles

--- app/js/main.js
+++ app/js/main.js
@@ -60,5 +60,17 @@ $("sup").on("click",function(){
 
 // --------------------------------------------------------------------------------------------------------------
 
+$(".head-faq").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+    });
+
+});
 
 
