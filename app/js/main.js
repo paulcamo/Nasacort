91fca6a22ebb7095eeebe371bbe0f0053b6e8336
Paulo Castillo <Paulo.Castillo@prodigious.com>
2015-08-17 16:21:47 -0600

    add new images

--- app/js/main.js
+++ app/js/main.js
@@ -66,6 +66,7 @@ $(".head-faq").click(function () {
     //getting the next element
     $content = $header.next();
     $header.find(".collp-icon").toggleClass("expanded-icon");
+    $header.find(".no-border-sect-faq").toggleClass("border-sect-faq");
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
