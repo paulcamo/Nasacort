c41040b1f5248d2f0c9fc4f606faa15360b56f70
Marlon Ulate <marlonjuc@hotmail.com>
2015-08-28 16:17:33 -0600

    NASACORTA-40 - Glass Heads Modal - Desktop

---
+++ app/js/sections/whats-a-clucker.js
@@ -0,0 +1,18 @@
+//JavaScript Document
+
+$(document).ready(function() {
+    
+    setGlassHeadsPopup();
+    
+});
+
+
+function setGlassHeadsPopup()
+{
+    $("body a").on("click",function() { 
+        if($(this).attr("data-type") === "maria")
+        {
+            
+        }
+    });
+}
