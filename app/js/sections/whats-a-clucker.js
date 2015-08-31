3b79e2e3f6e126e165642ee112d88328e8c0d323
Marlon Ulate <marlonjuc@hotmail.com>
2015-08-31 11:34:34 -0600

    NASACORTA-40

--- app/js/sections/whats-a-clucker.js
+++ app/js/sections/whats-a-clucker.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     
 });
 
+var mariasHotSpots = [{x:"17.1%",y:"28.30%",id:"itchy"},{x:"40.78%",y:"10.98%",id:"nasal"},{x:"37.74%",y:"32.84%",id:"congestion"}];
 
 function setGlassHeadsPopup()
 {
@@ -15,4 +16,34 @@ function setGlassHeadsPopup()
             
         }
     });
+    
+    addMariasHotSpots();
+}
+
+function addMariasHotSpots()
+{
+    for(var i = 0; i < mariasHotSpots.length; i++)
+    {
+         $("#glass-head #maria .hotspots-container").append("<div class='hotspot' id='"+ mariasHotSpots[i].id +"'></div>");
+         $("#glass-head #maria .hotspots-container").find("#"+ mariasHotSpots[i].id).css("left", mariasHotSpots[i].x).css("top", mariasHotSpots[i].y);
+    }
+    $("#glass-head #maria .hotspots-container .hotspot").each(function(index) {
+        $(this).on("click", function(){
+            resetHotSpots();
+            $(this).addClass("active");
+            hideMariasAnatomy();
+            var id = $(this).attr("id");
+            $("#glass-head #maria").find("." + id).show();
+        });
+    });
+}
+
+function hideMariasAnatomy()
+{
+    $("#glass-head #maria .anatomy").hide();
+}
+
+function resetHotSpots()
+{
+    $("#glass-head .hotspot").removeClass("active");
 }
