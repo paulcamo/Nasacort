1ff5d7bdcea0f078e874021399fd4cc2b2a8ed48
Marlon Ulate <marlonjuc@hotmail.com>
2015-09-01 12:16:53 -0600

    NASACORTA-40

--- app/js/sections/whats-a-clucker.js
+++ app/js/sections/whats-a-clucker.js
@@ -1,15 +1,16 @@
 //JavaScript Document
 
-$(document).ready(function() {
-    
+$(document).ready(function() {   
     setGlassHeadsPopup();
-    
 });
 
 var mariasHotSpots = [{x:"17.1%",y:"28.30%",id:"itchy"},{x:"40.78%",y:"10.98%",id:"nasal"},{x:"37.74%",y:"32.84%",id:"congestion"}];
 var marksHotSpots = [{x:"37.64%",y:"29.84%",id:"nasal"},{x:"25.49%",y:"40.30%",id:"histamine"}];
 var karasHotSpots = [{x:"27.94%",y:"28.30%",id:"congestion"},{x:"40.19%",y:"20%",id:"inflamation"}];
 var liamsHotSpots = [{x:"17.1%",y:"28.30%",id:"itchy"},{x:"40.78%",y:"10.98%",id:"nasal"},{x:"37.74%",y:"32.84%",id:"congestion"}];
+
+var currentCharacter = "";
+var currentGlassHead = "";
 
 function setGlassHeadsPopup()
 {
@@ -18,7 +19,12 @@ function setGlassHeadsPopup()
     $("body a").on("click",function() { 
         $("#glass-head .modal-header h1").text($(this).attr("title"));
         
-        hideAllAnatomies();
+        if(!isMobile)
+        {
+            hideAllAnatomies();
+        }
+        
+        currentCharacter = $(this).attr("data-type");
         
         if($(this).attr("data-type") === "maria")
         {
@@ -36,27 +42,39 @@ function setGlassHeadsPopup()
         {
             $("#glass-head #liam").show();
         }
+        if(currentGlassHead == "" && !isMobile)
+        { 
+             $("#glass-head #" + currentCharacter + " .default").show();   
+        }
+        if(currentGlassHead == "" && isMobile)
+        { 
+             showAllAnatomies();
+             $("#glass-head .default").hide();
+        }
+        
     });
     
     $("#glass-head #close").on("click",function(){
         $('#glass-head').modal('hide');
-        resetHotSpots();
-        resetAnatomies();
+        currentCharacter = "";
+        currentGlassHead = "";
+        if(!isMobile)
+        {
+            resetHotSpots();
+            resetAnatomies();
+        }else
+        {
+            hideAllAnatomies();
+        }
     });
     
-    addMariasHotSpots();
-    addMarksHotSpots();
-    addKarasHotSpots();
-    addLiamsHotSpots();
-}
-
-function hideAllAnatomies()
-{
-    $("#glass-head #maria").hide();
-    $("#glass-head #mark").hide();
-    $("#glass-head #kara").hide();
-    $("#glass-head #liam").hide();
-       
+    if(!isMobile)
+    {
+        addMariasHotSpots();
+        addMarksHotSpots();
+        addKarasHotSpots();
+        addLiamsHotSpots();
+    }
 }
 
 function addMariasHotSpots()
@@ -72,13 +90,14 @@ function addMariasHotSpots()
             $(this).addClass("active");
             hideMariasAnatomy();
             var id = $(this).attr("id");
+            currentGlassHead = id;
             $("#glass-head #maria").find("." + id).show();
         });
     });
 }
 
 function addMarksHotSpots()
-{
+{ 
     for(var i = 0; i < marksHotSpots.length; i++)
     {
          $("#glass-head #mark .hotspots-container").append("<div class='hotspot' id='"+ marksHotSpots[i].id +"'></div>");
@@ -90,6 +109,7 @@ function addMarksHotSpots()
             $(this).addClass("active");
             hideMarksAnatomy();
             var id = $(this).attr("id");
+            currentGlassHead = id;
             $("#glass-head #mark").find("." + id).show();
         });
     });
@@ -108,13 +128,14 @@ function addKarasHotSpots()
             $(this).addClass("active");
             hideKarasAnatomy();
             var id = $(this).attr("id");
+            currentGlassHead = id;
             $("#glass-head #kara").find("." + id).show();
         });
     });
 }
 
 function addLiamsHotSpots()
-{
+{ 
     for(var i = 0; i < liamsHotSpots.length; i++)
     {
          $("#glass-head #liam .hotspots-container").append("<div class='hotspot' id='"+ liamsHotSpots[i].id +"'></div>");
@@ -126,9 +147,19 @@ function addLiamsHotSpots()
             $(this).addClass("active");
             hideLiamsAnatomy();
             var id = $(this).attr("id");
+            currentGlassHead = id;
             $("#glass-head #liam").find("." + id).show();
         });
     });
+}
+
+function hideAllAnatomies()
+{
+    $("#glass-head #maria").hide();
+    $("#glass-head #mark").hide();
+    $("#glass-head #kara").hide();
+    $("#glass-head #liam").hide();
+       
 }
 
 function hideMariasAnatomy()
@@ -151,9 +182,30 @@ function hideLiamsAnatomy()
     $("#glass-head #liam .anatomy").hide();
 }
 
+function showAllAnatomies()
+{
+    if(currentCharacter != "")
+    {
+        $("#glass-head #" + currentCharacter + " .anatomy").show();
+    }
+}
+
+function showCurrentAnatomy()
+{
+    if(currentCharacter !=  "")
+    {
+        $("#glass-head #" + currentCharacter + " ." + currentGlassHead).show();   
+    }
+}
+
 function resetHotSpots()
 {
     $("#glass-head .hotspot").removeClass("active");
+}
+
+function hideHotSpots()
+{
+    $("#glass-head .hotspot").hide();
 }
 
 function resetAnatomies()
@@ -164,3 +216,56 @@ function resetAnatomies()
     hideLiamsAnatomy();
     $("#glass-head .default").show();
 }
+
+$(window).bind("resize", function(){
+
+    if(!isMobile)
+    {
+        if($('#glass-head #maria .hotspots-container').is(':empty')) 
+        {
+            addMariasHotSpots();
+            resetAnatomies();
+        }
+        
+        if($('#glass-head #mark .hotspots-container').is(':empty')) 
+        {  
+            addMarksHotSpots();
+            resetAnatomies();
+        }
+        if($('#glass-head #kara .hotspots-container').is(':empty')) 
+        {
+            addKarasHotSpots();
+            resetAnatomies();
+        }
+        if($('#glass-head #liam .hotspots-container').is(':empty')) 
+        {
+            addLiamsHotSpots();
+            resetAnatomies();
+        }
+        if(currentGlassHead != "" && currentCharacter != "")
+        { 
+            if(currentCharacter != "")
+            {
+                $("#glass-head #" + currentCharacter + " .anatomy").hide();  
+            }
+             showCurrentAnatomy();   
+        }
+        if(currentGlassHead == "")
+        {
+            resetAnatomies();
+        }
+        
+        $("#glass-head .hotspot").show();
+        if(currentGlassHead != "")
+        {
+            $("#glass-head .hotspots-container #" + currentGlassHead).addClass("active");
+        }
+
+    }else
+    { 
+        resetHotSpots();
+        hideHotSpots();
+        showAllAnatomies();
+        $("#glass-head .default").hide();   
+    }
+});
