3697788608114a29b14e4c64960a4f38db0de9fa
Marlon Ulate <marlonjuc@hotmail.com>
2015-09-03 16:20:30 -0600

    Resources Page Build / Mobile/Fluid

---
+++ app/js/sections/resources.js
@@ -0,0 +1,64 @@
+//JavaScript Document
+
+var minDesktopScreen = 1400;
+var minTabletScreen = 1024;
+
+$(document).ready(function(){   
+    keepAllContainersSameHeight();
+});
+
+$(window).bind("resize", function(){
+    keepAllContainersSameHeight();
+});
+
+function keepAllContainersSameHeight()
+{
+    var biggerHeightPatients = 0;
+    var biggerHeightPractice = 0;
+    var contentwidth = $(window).width();
+
+    if(isMobile)
+    {
+        biggerHeightPatients = "auto";
+        biggerHeightPractice = "auto";
+    }else
+    {
+        if(contentwidth < minDesktopScreen)
+        {
+            
+            $(".for-your-patients .resource-container").each(function(index){
+                if($(this).innerHeight() > 198)
+                {
+                    if($(this).innerHeight() >= biggerHeightPatients)
+                    {
+                       biggerHeightPatients = $(this).innerHeight(); 
+                    }
+                }
+            });
+            $(".for-your-practice .resource-container").each(function(index){
+                if($(this).innerHeight() > 294)
+                {
+                    if($(this).innerHeight() >= biggerHeightPractice)
+                    {
+                       biggerHeightPractice = $(this).innerHeight(); 
+                       if(contentwidth < minTabletScreen)
+                        {
+                             biggerHeightPractice = "auto";
+                        }
+                    }
+                }
+            });
+        }
+        else
+        {
+            biggerHeightPatients = "auto";
+            biggerHeightPractice = "294px";
+        } 
+    }
+    $(".for-your-patients .resource-container").each(function(index){
+        $(this).css("min-height", biggerHeightPatients);
+    });
+    $(".for-your-practice .resource-container").each(function(index){
+        $(this).css("min-height", biggerHeightPractice);
+    });
+}
