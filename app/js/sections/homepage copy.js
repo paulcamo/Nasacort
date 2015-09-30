d236d17b24ebf85a817b17092feac2ab287cfaa8
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2015-09-30 13:23:16 -0600

    WIP desktop

--- app/js/sections/homepage copy.js
+++ app/js/sections/homepage copy.js
@@ -56,8 +56,6 @@ $(document).ready(function() {
 
     var tl1 = new TimelineMax({repeat:-1});
     var tl2 = new TimelineLite();
-
-
     var tl_mobile = new TimelineMax({repeat:-1});
 
 
@@ -119,126 +117,39 @@ $(document).ready(function() {
     // ----- to go to slide 2 -------------------------------------------------------------------------------------------------------------------------------
 
 
-    $("#sec02").on("click",function(e) {
-        e.preventDefault();
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide1-textbox'), 0.5, {top: pixel01 },        {top:'-900px'}),     30000);
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide01'), 0.5, {top: pixel01 },        {top:'-900px'}),     30000);
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#herolink'), 0.5, {top:'0px'},        {top:'-900px'}),     30000);
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide02'), 0.5, {marginTop:'0em'},    {marginTop:'-15em'}),     30000);
 
-        var tl000 = new TimelineMax();
-
-
-
-        tl000
-            .to('#slide1-textbox', 3,    {top: '-900px',  ease: Sine. easeIn} , '+=0')
-            .to('#slide01', 3,           {top: '-900px',  ease: Sine. easeIn} , '-=3')
-            .to('#background', 3,        {top: '-10.7em',  ease: Sine. easeIn} , '-=3')
-            .fromTo($('#herolink'), 3,   {top: '0px'}, {top: '-900px'} , '-=3')
-            .to('#slide02', 4,           {marginTop: '-15em',  ease: Sine. easeInOut} , '-=6')
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#bottle'), 0.5, {marginTop: pixel02 , scale:'1', left:'72%'  },   {marginTop:'17em', scale:'2.5', left:'28.1%' , ease:  Power1.easeOut}  ),     35000, 5000);  //bottle movement
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#cap'), 0.5, {marginTop:'-0.2em', scale:'1', left:'74.6%', height: '3em'  },   {marginTop:'14.4em', scale:'2.5', left:'30.5%' , height: '4em', ease:  Power1.easeOut}  ),     35000, 5000);  //bottle movement
 
 
+    controller.addTween('#marker-tr', TweenMax.to( $('#cap'), 0.5,   {marginTop:'7.5em' , ease:  Power1.easeOut}  ),     35000, 45000); // opening cap...
+    controller.addTween('#marker-tr', TweenMax.to( $('#cap'), 0.5,   {marginTop:'16.7em', left:'92%' , rotation: 35, ease:Cubic.easeInOut }  ),     35000, 65000);
 
-
-            .to($('#bottle'), 6,           {marginTop: '17em',scale: '2.5',left: '28.1%',ease: Power1.easeOut } , '-=6')
-            .to($('#cap'), 6,               {marginTop: '14.4em', scale: '2.5', left: '30.5%', height: '4em', ease: Power1.easeOut} , '-=6')
-
-            .to($('#cap'), 0.7,                  {marginTop: '7.5em', ease: Power1.easeOut} , '+=0')
-            .to($('#cap'), 1,                  {marginTop: '16.7em', left: '92%', rotation: 35, ease: Cubic.easeInOut} , '+=0')
-            .to($('#no-scent'), 0.8,             {height: '8em'} , '+=0')
-            .fromTo($('#nasacort-compare'), 1.5, {right: '-35%'}, {right: '20.4%', ease: Power1.easeOut} , '+=0')
-
-
-        ;
-
-
-    });
-
-    //
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide1-textbox'), 0.5, {top: pixel01 },        {top:'-900px'}),     30000);
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide01'), 0.5, {top: pixel01 },        {top:'-900px'}),     30000);
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#herolink'), 0.5, {top:'0px'},        {top:'-900px'}),     30000);
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#slide02'), 0.5, {marginTop:'0em'},    {marginTop:'-15em'}),     30000);
-    //
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#bottle'), 0.5, {marginTop: pixel02 , scale:'1', left:'72%'  },   {marginTop:'17em', scale:'2.5', left:'28.1%' , ease:  Power1.easeOut}  ),     35000, 5000);  //bottle movement
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#cap'), 0.5, {marginTop:'-0.2em', scale:'1', left:'74.6%', height: '3em'  },   {marginTop:'14.4em', scale:'2.5', left:'30.5%' , height: '4em', ease:  Power1.easeOut}  ),     35000, 5000);  //bottle movement
-    //
-    //
-    //controller.addTween('#marker-tr', TweenMax.to( $('#cap'), 0.5,   {marginTop:'7.5em' , ease:  Power1.easeOut}  ),     35000, 45000); // opening cap...
-    //controller.addTween('#marker-tr', TweenMax.to( $('#cap'), 0.5,   {marginTop:'16.7em', left:'92%' , rotation: 35, ease:Cubic.easeInOut }  ),     35000, 65000);
-    //
-    //controller.addTween('#marker-tr', TweenMax.to( $('#background'),  0.5, {top:'-10.7em'}),     20000, 110000);
-    //controller.addTween('#marker-tr', TweenMax.to( $('#no-scent'),  0.5, {height:'8em'}),     35000, 80000);
-    //controller.addTween('#marker-tr', TweenMax.fromTo( $('#nasacort-compare'), 0.5, {right:'-35%' },   {right:'20.4%' , ease:  Power1.easeOut}  ),     35000, 120000);
-    //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    controller.addTween('#marker-tr', TweenMax.to( $('#background'),  0.5, {top:'-10.7em'}),     20000, 110000);
+    controller.addTween('#marker-tr', TweenMax.to( $('#no-scent'),  0.5, {height:'8em'}),     35000, 80000);
+    controller.addTween('#marker-tr', TweenMax.fromTo( $('#nasacort-compare'), 0.5, {right:'-35%' },   {right:'20.4%' , ease:  Power1.easeOut}  ),     35000, 120000);
 
 
 
     // ----- to go to slide 3 -------------------------------------------------------------------------------------------------------------------------------
 
-    //  controller.addTween('#marker8-tr', TweenMax.to( $('#slide03'),            1,   {top:'1.6em'}),     0);
-
-
-
-    $("#sec03").on("click",function(e) {
-        e.preventDefault();
-
-        var tl000 = new TimelineMax();
-
-        tl000
-            .to('#background', 3, {top: '-33em', ease: Power2.easeOut}, '+=0')
-            .to('#slide02', 3, {marginTop: '-34em', ease: Power1.easeOut}, '-=3')
-            .to('#compare-copy', 1, {left: '23%'}, '+=0')
-            .to('#compare-bg', 1,  {top: '1.4em'}, '+=0')
-            .to('#stats', 1,  {height: '30.5%'}, '+=0')
-        ;
-
-
-    });
-
-
+  //  controller.addTween('#marker8-tr', TweenMax.to( $('#slide03'),            1,   {top:'1.6em'}),     0);
+    controller.addTween('#marker8-tr', TweenMax.to( $('#background'),            1,   {top:'-33em'}),     30000);
+    controller.addTween('#marker8-tr', TweenMax.to( $('#slide02'),            1,   {marginTop:'-34em'}),     30000);
+    controller.addTween('#marker8-tr', TweenMax.fromTo( $('#compare-bg'),            1, {top:'17em'},    {top:'1.4em'}),    35000, 65000);
+    controller.addTween('#marker8-tr', TweenMax.fromTo( $('#compare-copy'),          1, {left:'-42%'},   {left:'23%'}),      35000, 31000);
+    controller.addTween('#marker8-tr', TweenMax.fromTo( $('#stats'),                 1, {height:'0%'},   {height:'30.5%'}),    15000, 90000);
 
     // ------ to go to slide 4 ------------------------------------------------------------------------------------------------------------------------
 
-
-    $("#sec03").on("click",function(e) {
-        e.preventDefault();
-
-        var tl000 = new TimelineMax();
-
-        tl000
-
-   .to('#background',   1,  {top:'-53em'})
-   .to('#slide03',      1,  {top:'-16em'})
-   .to('#slide04',      1,  {marginTop:'0em'})
-   .to('#patient-box',  3,  {right:'0%'})
-   ;
-
-
-    });
-
-
-    //
-    //.to('#slide03',               1, {top:'1.5em'},       {top:'-16em'}),     30000);
-    //.to('#slide04',               1, {marginTop:'17em'},  {marginTop:'0em'}), 25000);
-    //.to('#patient-box',           1, {right:'-180%'},     {right:'0%'}),      30000, 25000);
-    //
-    //
-
-
+    controller.addTween('#marker9-tr', TweenMax.to( $('#background'),            1,   {top:'-53em'}),     30000);
+    controller.addTween('#marker9-tr', TweenMax.fromTo( $('#slide03'),               1, {top:'1.5em'},       {top:'-16em'}),     30000);
+    controller.addTween('#marker9-tr', TweenMax.fromTo( $('#slide04'),               1, {marginTop:'17em'},  {marginTop:'0em'}), 25000);
+    controller.addTween('#marker9-tr', TweenMax.fromTo( $('#patient-box'),           1, {right:'-180%'},     {right:'0%'}),      30000, 25000);
 
     // ------ to go to slide 5 ------------------------------------------------------------------------------------------------------------------------
 
@@ -261,9 +172,9 @@ $(document).ready(function() {
 
 
 $(window).bind("resize", function(){
-    set_dotbox();
-    set_footer_width();
-    set_variables();
+set_dotbox();
+set_footer_width();
+set_variables();
 
 
     controller.triggerCheckAnim(false);
@@ -343,12 +254,12 @@ $(window).scroll(function() {
         console.log("bottom!");
 
 
-        // $("body").css("padding-bottom","1000px");
+      // $("body").css("padding-bottom","1000px");
 
-        //  $("#background").css("top","-100px");
+      //  $("#background").css("top","-100px");
 
 
-    }
+        }
 
 
 
@@ -362,17 +273,14 @@ $("#dots-container .dotbox .dot").on("click",function(e){
     $(this).addClass("dot-on");
 
 
-//    var lugar =  "#" + $(this).data("section");
-//    var time = $(this).data("time") || 5000;
+    var lugar =  "#" + $(this).data("section");
+    var time = $(this).data("time") || 5000;
 
     //  alert (time);
 
-    //$('html, body').animate({
-    //    scrollTop: $(lugar).offset().top
-    //}, time);
-    //
-
-
+    $('html, body').animate({
+        scrollTop: $(lugar).offset().top
+    }, time);
 });
 
 // ---------------------------------------------------------------------------------------
