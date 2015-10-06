cfef071fe42448b2ede2ce7418323443d687189c
Marcelo Guzman <marcelo.guzman@prodigious.cr>
2015-10-06 13:13:36 -0600

    script for tagging faqs page

--- app/js/sections/faq.js
+++ app/js/sections/faq.js
@@ -8,20 +8,27 @@ $(".head-faq").click(function(){
     if (state=="close") {
         $(this).data("state", "open");
         console.log (item + " is open!!!");
+        click_expand(item);
+
 
     } else {
         $(this).data("state", "close");
         console.log (item  + " is close");
+        click_close(item);
     }
 
 
     // --------------------------------------------
+function click_expand(item) {
+
+    console.log (item + " expand fired");
+
     switch(item) {
 
 
         case "xx1":
             // ga for xx1
-             break;
+            break;
         case "xx2":
             // ga for xx2
             break;
@@ -31,9 +38,31 @@ $(".head-faq").click(function(){
 
 
     }
+}
     // --------------------------------------------
 
+    function click_close(item) {
 
+        console.log (item + " close fired");
+
+
+        switch(item) {
+
+
+            case "xx1":
+                // ga for xx1
+                break;
+            case "xx2":
+                // ga for xx2
+                break;
+
+
+
+
+
+        }
+    }
+    // --------------------------------------------
 
 
 
